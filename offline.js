ef9c03cceecbd6dec513a348b4583284b70d68dc
Kallpolo <mantapjiwa250104@gmail.com>
2023-03-21 20:41:07 +0700

    paccircle

---
+++ offline.js
@@ -0,0 +1,33 @@
+﻿{
+	"version": 1641951463,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/buttonstart-sheet0.png",
+		"images/background-sheet0.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/points-sheet0.png",
+		"images/texts.png",
+		"images/effectplayer-sheet0.png",
+		"images/enemy-sheet0.png",
+		"images/particlesenemy.png",
+		"images/particlesplayer1.png",
+		"images/particlesplayer2.png",
+		"images/dev-sheet0.png",
+		"images/logo-sheet0.png",
+		"media/lose.ogg",
+		"media/start.ogg",
+		"media/best.ogg",
+		"media/point.ogg",
+		"media/enemy.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
